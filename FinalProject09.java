bf1c298d3ade604d29b7af73fdd3410878ce3c8e
Dzy-hue <diazprahadyan999@gmail.com>
2025-10-02 12:48:00 +0700

    Perubahan 2

--- FinalProject09.java
+++ FinalProject09.java
@@ -1,5 +1,6 @@
 public class FinalProject09 {
 public static void main(String[] args) {
     System.out.println("Hi, ini baris kode pertama");
+    System.out.println("Saya Diaz sebagai pemilik project");
     }
 }
